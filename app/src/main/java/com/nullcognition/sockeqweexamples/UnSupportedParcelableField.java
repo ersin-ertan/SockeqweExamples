7ba33235e05e7d22d27ede443eeb69dd8b06b660
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-08-01 22:45:59 -0400

    removed the app module, initial commit of the worked example, included butter knife

--- app/src/main/java/com/nullcognition/sockeqweexamples/UnSupportedParcelableField.java
+++
@@ -1,8 +0,0 @@
-package com.nullcognition.sockeqweexamples;// Created by ersin on 18/07/15
-
-public class UnSupportedParcelableField{
-
-	public UnSupportedParcelableField(int ii){i = ii;}
-
-	int i = 8;
-}
---
+++ workedarchitecture/src/main/java/com/nullcognition/workedarchitecture/ItemDetailActivity.java
@@ -0,0 +1,38 @@
+package com.nullcognition.workedarchitecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class ItemDetailActivity extends AppCompatActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_item_detail);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		if(savedInstanceState == null){
+			Bundle arguments = new Bundle();
+			arguments.putString(ItemDetailFragment.ARG_ITEM_ID,
+					getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID));
+			ItemDetailFragment fragment = new ItemDetailFragment();
+			fragment.setArguments(arguments);
+			getSupportFragmentManager().beginTransaction()
+			                           .add(R.id.item_detail_container, fragment)
+			                           .commit();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		int id = item.getItemId();
+		if(id == android.R.id.home){
+			NavUtils.navigateUpTo(this, new Intent(this, ItemListActivity.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
